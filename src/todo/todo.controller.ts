68344c42631c3c135ed64ab8a8f9b4d82240f7d2
aymen2782 <aymen.sellaouti@gmail.com>
2021-02-08 14:33:24 +0100

    todo add

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Todo } from './models/todo';
+import { TodoStatusEnum } from './enums/TodoStatusEnum';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('todo')
 export class TodoController {
@@ -8,5 +10,28 @@ export class TodoController {
   @Get('')
   getTodos(): Todo[] {
     return this.todos;
+  }
+
+  @Post()
+  addTodo(
+    @Body() todoData
+  ): Todo {
+    console.log(todoData);
+    const {name, description} = todoData;
+    /*
+    * 1- Récupérer les infos envoyés par le user avec @Body
+    * 2- Créer (instanicer) un todo
+    * 3- Ajouter les infos manquantes
+    * 4- L'ajouter dans le tableau
+    * 5- Retourner l'objet todo crée
+    * */
+    const todo = new Todo();
+    todo.description = description;
+    todo.name = name;
+    todo.date = new Date();
+    todo.status = TodoStatusEnum.waiting;
+    todo.id = uuidv4();
+    this.todos.push(todo);
+    return todo;
   }
 }
